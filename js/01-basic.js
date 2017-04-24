45a322042a9ea6090ee44842984a9d8e604f0ebf
AlmasB <almaslvl@gmail.com>
2017-04-24 21:45:17 +0100

    class test

--- js/01-basic.js
+++ js/01-basic.js
@@ -1,6 +1,30 @@
+class Rectangle {
+    constructor(width, height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    get area() {
+        return this.calcArea();
+    }
+
+    calcArea() {
+        return this.height * this.width;
+    }
+}
+
+const rect = new Rectangle(800, 600);
+
 var game;
 
 function main() {
+    
+    console.log(rect.area);
+    
+    console.log(rect.width);
+    
+    console.log(rect.height);
+    
     game = new Phaser.Game(800, 600, Phaser.AUTO, '', { create: create });
 }
 
