516c11afc5de0849fddb195fa65fb4148b3393bc
AlmasB <almaslvl@gmail.com>
2017-12-17 18:39:15 +0000

    added sem2 base code

---
+++ sem2/client/js/game.js
@@ -0,0 +1,41 @@
+var Game = {};
+
+Game.init = function(){
+    game.stage.disableVisibilityChange = true;
+};
+
+Game.preload = function() {
+    game.load.image('sprite', 'assets/coin.png');
+};
+
+Game.create = function(){
+    Game.playerMap = {};
+    var testKey = game.input.keyboard.addKey(Phaser.Keyboard.ENTER);
+    testKey.onDown.add(Client.sendTest, this);
+    
+    game.input.onTap.add(Game.getCoordinates, this);
+    
+    Client.askNewPlayer();
+};
+
+Game.getCoordinates = function(pointer){
+    Client.sendClick(pointer.worldX,pointer.worldY);
+};
+
+Game.addNewPlayer = function(id,x,y){
+    Game.playerMap[id] = game.add.sprite(x,y,'sprite');
+};
+
+Game.movePlayer = function(id,x,y){
+    var player = Game.playerMap[id];
+    var distance = Phaser.Math.distance(player.x,player.y,x,y);
+    var tween = game.add.tween(player);
+    var duration = distance*10;
+    tween.to({x:x,y:y}, duration);
+    tween.start();
+};
+
+Game.removePlayer = function(id){
+    Game.playerMap[id].destroy();
+    delete Game.playerMap[id];
+};
