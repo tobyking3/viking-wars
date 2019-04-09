bec7fa5bbe1b8175894963ddc5a94e7885b377fd
Jake Fauvel <jakefauvel@msn.com>
2019-04-09 18:56:09 +0100

    Boot preload fix

--- game/client/js/game.js
+++ game/client/js/game.js
@@ -23,6 +23,18 @@ Game.init = function() {
     game.stage.disableVisibilityChange = true;
 };
 
+Game.preload = function() {
+    game.load.image('background', 'assets/backgrounds/long_scene.png');
+    game.load.image('waiting', 'assets/waiting.png');
+    game.load.atlas('brown_viking', 'assets/brown_viking.png', 'assets/brown_viking.json', Phaser.Loader.TEXTURE_ATLAS_JSON_HASH);
+    game.load.atlas('red_viking', 'assets/red_viking.png', 'assets/red_viking.json', Phaser.Loader.TEXTURE_ATLAS_JSON_HASH);
+    game.load.image('arrow', './assets/left_arrow.png');
+    game.load.image('turret_player_1', './assets/turret-player1.png');
+    game.load.image('turret_player_2', './assets/turret-player2.png');
+    game.load.image('ground', './assets/ground.png');
+    game.load.spritesheet('ready_button', 'assets/ready.png', 1000, 365);
+};
+
 Game.create = function() {
     Game.playerMap = {};
     Game.vikingMap = {};
